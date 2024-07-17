2a08cfdb3550f23642e20dbecf4b5cd1260e69a6
shibsrc <shibjyoti25@gmail.com>
2024-07-17 15:47:48 +0530

    sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,33 +1,7 @@
-window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    deferredPrompt = event;
-    showInstallButton();
-  });
+self.addEventListener('install', function(event) {
+    
+});
+
+self.addEventListener('fetch', function(event) {
   
-  function showInstallButton() {
-    installButton.style.display = 'block';
-  }
-  
-  installButton.addEventListener('click', () => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-      deferredPrompt = null;
-    });
-  });
-  
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-          console.log('Service Worker registered: ', registration);
-        })
-        .catch((error) => {
-          console.error('Service Worker registration failed: ', error);
-        });
-    });
-  }
+});
